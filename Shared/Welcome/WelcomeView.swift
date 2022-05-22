4d8a97387aff38070cc0e080c0cf0bd92d00e8ee
Alex <a.zubach@finch-melrose.com>
2022-05-22 21:09:49 +0300

    Stopped on page 180

--- Shared/Welcome/WelcomeView.swift
+++ Shared/Welcome/WelcomeView.swift
@@ -33,44 +33,21 @@
 import SwiftUI
 
 struct WelcomeView: View {
-  var body: some View {
-    ZStack {
-      Image("welcome-background", bundle: nil)
-        .resizable()
-        .aspectRatio(1 / 1, contentMode: .fill)
-        .edgesIgnoringSafeArea(.all)
-        .saturation(0.5)
-        .blur(radius: 5)
-        .opacity(0.08)
-      
-      Label {
-        VStack(alignment: .leading) {
-          Text("Welcome to")
-            .font(.headline)
-            .bold()
-          Text("Kuchi")
-            .font(.largeTitle)
-            .bold()
+    var body: some View {
+        ZStack {
+            WelcomeBackgroundImage()
+            Label {
+                WelcomeMessageView()
+            } icon: {
+                LogoImage()
+            }
+            .labelStyle(HorizontallyAlignedLabelStyle())
         }
-        .foregroundColor(.red)
-        .lineLimit(1)
-        .padding(.horizontal)
-      } icon: {
-        Image(systemName: "table")
-          .resizable()
-          .frame(width: 30, height: 30)
-          .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-          .background(Color(white: 0.9))
-          .clipShape(Circle())
-          .foregroundColor(.red)
-      }
-      .labelStyle(HorizontallyAlignedLabelStyle())
     }
-  }
 }
 
 struct WelcomeView_Previews: PreviewProvider {
-  static var previews: some View {
-    WelcomeView()
-  }
+    static var previews: some View {
+        WelcomeView()
+    }
 }
